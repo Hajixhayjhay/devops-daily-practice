5a9b69a31d2a38d24ca96acb253cffa4d82be29e
Hajarat <akandehajarat@gmail.com>
2026-03-12 11:13:44 +0000

    updated backup file

--- backup.sh
+++ backup.sh
@@ -11,6 +11,11 @@ if [ $? -eq 0 ]; then
 echo "File copied successfully!"
 echo "$(date): Backup successfully - $DEST" | sudo tee -a $LOGFILE
 aws s3 cp $DEST s3://nginx-backup-hajarat/ --recursive
+if [ $? -eq 0 ]; then
+echo "$(date): s3 upload successful" | sudo tee -a $LOGFILE
+else
+echo "$(date): s3 upload failed" | sudo tee -a $LOGFILE
+fi
 else
 echo "Error!: failed to copy files"
 echo "$(date): Backup failed - $DEST" | sudo tee -a $LOGFILE
